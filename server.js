074e4345eb6dd19c9311b02b1389e47245afa11d
Anil S <nil.kul007@gmail.com>
2022-06-07 11:24:04 +0530

    Port updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./app");
 
 const server = http.createServer(app);
-
+const PORT=process.env.PORT || 3000
 server.listen(3000, () => {
   console.log("Server is running on PORT: 3000");
 });
